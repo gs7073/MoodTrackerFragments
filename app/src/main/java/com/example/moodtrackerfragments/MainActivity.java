6aa4c9b62107990462560b38cb2edd7af15b7525
Cyber_Mamriot <gs7073@bs.amalnet.k12.il>
2026-02-09 14:14:33 +0200

    final

--- app/src/main/java/com/example/moodtrackerfragments/MainActivity.java
+++ app/src/main/java/com/example/moodtrackerfragments/MainActivity.java
@@ -1,12 +1,9 @@
 package com.example.moodtrackerfragments;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +12,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+
+        // הגדרת מאזין ללחיצות בתפריט
+        bottomNav.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            int id = item.getItemId();
+
+            // בדיקה איזה כפתור נלחץ
+            if (id == R.id.nav_happy) {
+                selectedFragment = new HappyFragment();
+            } else if (id == R.id.nav_neutral) {
+                selectedFragment = new NeutralFragment();
+            } else if (id == R.id.nav_sad) {
+                selectedFragment = new SadFragment();
+            }
+
+            // החלפת הפרגמנט בפועל
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, selectedFragment)
+                        .commit();
+            }
+            return true;
+        });
+
+        // הצגת פרגמנט ברירת מחדל בכניסה לאפליקציה
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new HappyFragment())
+                    .commit();
+        }
     }
 }
